6d8ead04b35d61f3261cc0c71c5df1d153f20c31
Hari Vinod <harivinod1018@gmail.com>
2025-09-07 14:13:42 +0530

    Add React build to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,34 +9,26 @@ import "./styles.css";
 function App() {
   const [links, setLinks] = useState([]);
 
-  // Load saved links from localStorage
   useEffect(() => {
     const saved = JSON.parse(localStorage.getItem("shortLinks")) || [];
     setLinks(saved);
   }, []);
 
-  // Save links whenever they change
   useEffect(() => {
     localStorage.setItem("shortLinks", JSON.stringify(links));
   }, [links]);
 
   const handleShorten = async (url, timeLimit) => {
     try {
-      const response = await fetch(
-        "https://shortener-url-hf6h.onrender.com/api/shorten",
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ url, timeLimit }),
-        }
-      );
+      const response = await fetch("http://localhost:5000/api/shorten", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ url, timeLimit: Number(timeLimit) }),
+      });
       if (!response.ok) throw new Error("Failed to shorten");
       const data = await response.json();
 
-      setLinks((prev) => {
-        const newLinks = [data.shortUrl, ...prev];
-        return newLinks.slice(0, 5); // Keep only last 5
-      });
+      setLinks((prev) => [data.shortUrl, ...prev].slice(0, 5));
 
       return data.shortUrl;
     } catch (err) {
@@ -53,8 +45,6 @@ function App() {
 
   return (
     <div className="app-container">
-      {/* <h1 className="app-title">URL Shortener with Expiry</h1> */}
-
       <LinkForm onShorten={handleShorten} />
 
       <div className="shortlink-list">
